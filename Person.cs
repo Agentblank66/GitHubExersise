74c81ec15df9ae8cbb1d2c5e0de508748c4dea73
PowerBasss447 <Kasper.sandholt@outlook.com>
2024-11-13 13:41:15 +0100

    New

--- Person.cs
+++ Person.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 public class Person
 {
+    public string Name { get; set; }
+    public int Id { get; set; }
 
+    public Person(string name, int id)
+    {
+        Name = name;
+        Id = id;
+    }
 
+    public override string ToString()
+    {
+        return $"Name: {Name}" + $"Id: {Id}";
+
+    }
 }
-
