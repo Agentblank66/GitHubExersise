19cb8a7acf2619c6dbb1e40006dfb0764ea30740
PowerBasss447 <Kasper.sandholt@outlook.com>
2024-11-13 13:42:08 +0100

    Bas

--- Person.cs
+++ Person.cs
@@ -8,11 +8,13 @@ public class Person
 {
     public string Name { get; set; }
     public int Id { get; set; }
+    public string FirstName { get; set; }
 
     public Person(string name, int id)
     {
         Name = name;
         Id = id;
+
     }
 
     public override string ToString()
